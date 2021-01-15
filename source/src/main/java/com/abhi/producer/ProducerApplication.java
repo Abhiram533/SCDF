f9c9d7363789399eeac22eb1c882c52c5da29757
Abhiram533 <abhisjune4@gmail.com>
2021-01-15 08:34:23 +0530

    Update ProducerApplication.java

--- source/src/main/java/com/abhi/producer/ProducerApplication.java
+++ source/src/main/java/com/abhi/producer/ProducerApplication.java
@@ -24,7 +24,7 @@ public class ProducerApplication {
 	}
 	@Bean
 	@Primary
-	@ConfigurationProperties("spring.datasource")
+	@ConfigurationProperties(prefix="spring.datasource")
 	public DataSourceProperties memberDataSourceProperties() {
 	    return new DataSourceProperties();
 	}
